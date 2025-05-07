7cd5c7312d6f3014c57441b1968da0b188451e0e
decryptedchaos <nixgod@gmail.com>
2025-05-07 18:06:06 -0500

    Remove .idea
    Clean up code
    
    Add more module examples

--- modc.cpp
+++ modc.cpp
@@ -4,8 +4,13 @@
 import mymath;
 import hello;
 import hermath;
+import holyshit;
 
 int main() {
-	std::cout << helloWorld::hello() << " 2 + 3 is " << mymath::add(2, 3)  << " What $20,000 Looks like to your girlfriend " << girlmath::girlfriend(20000) << '\n';
+	std::cout << helloWorld::hello()
+		  << "\n2 + 3 = " << mymath::add(2, 3)
+		  <<  "\nWhat $20,000 Looks like to your girlfriend " << girlmath::girlfriend(20000)
+		  << "\n5 + 1 = " << wellshit::plusOne(5)
+		  << "\n5 - 1 = " << toInfinity::minusOne(5) << '\n';
 	return 0;
 }
---
+++ modules/holyshit.ixx
@@ -0,0 +1,23 @@
+//
+// Created by josh on 5/7/25.
+//
+module;
+
+export module holyshit;
+
+export namespace wellshit
+{
+
+    int plusOne(int n)
+    {
+        return ++n ;
+    }
+
+}
+
+export namespace toInfinity{
+    int minusOne(int n)
+    {
+        return --n;
+    }
+}
