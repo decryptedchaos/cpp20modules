4a3d55f80688e473b6cd2f4814cfa5c7e87072e9
decryptedchaos <nixgod@gmail.com>
2025-05-07 23:36:45 -0500

    Add a few comments to explain modules

--- modc.cpp
+++ modc.cpp
@@ -1,4 +1,7 @@
 // Simple Hello World program
+
+//in C++20 we still include system headers in every file that needs them C++23 lets you import system headers like you do modules but brings its own
+// set of issues to deal with
 #include <iostream>
 
 import mymath;
@@ -8,9 +11,9 @@ import holyshit;
 
 int main() {
 	std::cout << helloWorld::hello()
-		  << "\n2 + 3 = " << mymath::add(2, 3)
-		  <<  "\nWhat $20,000 Looks like to your girlfriend " << girlmath::girlfriend(20000)
-		  << "\n5 + 1 = " << wellshit::plusOne(5)
-		  << "\n5 - 1 = " << toInfinity::minusOne(5) << '\n';
+			  << "\n2 + 3 = " << mymath::add(2, 3)
+			  <<  "\nWhat $20,000 Looks like to your girlfriend " << girlmath::girlfriend(20000)
+			  << "\n5 + 1 = " << wellshit::plusOne(5)
+			  << "\n5 - 1 = " << toInfinity::minusOne(5) << '\n';
 	return 0;
 }
--- modules/holyshit.ixx
+++ modules/holyshit.ixx
@@ -1,6 +1,5 @@
-//
-// Created by josh on 5/7/25.
-//
+// There does not seem to be a set place where to put this module; section,  some put it here some put it under the export module
+// The key point to understand is this is required in modules that use #include for normal headers.
 module;
 
 export module holyshit;
